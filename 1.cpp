21b34f3727ecb77174471f4a1fae99f315cae00c
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2025-03-12 23:34:40 -0500

    Update 1.cpp
    xd

--- 1.cpp
+++ 1.cpp
@@ -1,125 +1,106 @@
 #include <iostream>
 using namespace std;
 
-
-
-struct Vector
-{
-       
-        int* m_pVect, // Pointer to the buffer
-        m_nCount, // Control how many elements are actually used
-        m_nMax, // Control how many are allocated as maximum
-        m_nDelta; // To control the growing
+struct Vector {
+    int* m_pVect;
+    int m_nCount;
+    int m_nMax;
+    int m_nDelta;
 };
 
-
-void constructior(int*& m, int& m1,int& m2,int& m3, int& m4) {
-    
-
-
+void constructor(int*& m, int& m1, int& m2, int& m3, int& m4) {
+    m1 = 0;
+    m2 = 10;
+    m3 = 10;
+    m = new int[m2];
+    m4 = m2;
 }
 
-void Resize(int*& Vect, int& Max)
-{
-    const int delta = 10; // Used to increase the vector size
-     int *pTemp, i;
-     pTemp = new int[Max + delta]; // Alloc a new vector
-     for(i = 0 ; i < Max ; i++) // Transfer the elements
-            pTemp[i] = Vect[i]; // we can also use the function memcpy
-        delete [ ] Vect; // delete the old vector
-        Vect = pTemp;
-        Max += delta; // The Max has to be increased by delta
-}
-void Insert(int*& rpVect, int& rnCount, int& rnMax, int elem)
-{
-    if (rnCount == rnMax) // Verify the overflow
-        Resize(rpVect, rnMax); // Resize the vector before inserting elem
-    rpVect[rnCount++] = elem; // Insert the element at the end of the sequence
+void Resize(int*& Vect, int& Max) {
+    const int delta = 10;
+    int* pTemp = new int[Max + delta];
+    for (int i = 0; i < Max; i++)
+        pTemp[i] = Vect[i];
+    delete[] Vect;
+    Vect = pTemp;
+    Max += delta;
 }
 
-
+void Insert(int*& rpVect, int& rnCount, int& rnMax, int elem) {
+    if (rnCount == rnMax)
+        Resize(rpVect, rnMax);
+    rpVect[rnCount++] = elem;
+}
 
 template<class T>
-class  CVector {
-
-    public:
-        CVector(int delta = 10) {
-            tammax = delta;
-            head = new T[tammax];
-            ptr = head;
-        }
-        void expan() {
-            int h = 10;
-            T* ptr1 = new T[tammax+h];
-            for (int i = 0; i < tammax; i++) // Transfer the elements
-                ptr1[i] = head[i];
-            tammax += h;
-            delete[] head;
-            head = ptr1;
-            ptr = head;
-
-
-        }
-        void Insert(T elem) {
-            if (tam == tammax)
-                expan();
-            *(ptr + tam) = elem;
-            tam++;
-
-        }
-        void print() {
-            T* f = head;
-            for (int i = 0; i < tam; i++) {
-                cout << *(f + i) << " ";
-            }
-            cout << endl;
-        }
-
-    private:
-        T* head;
-        T* ptr;
-        int tam;
-        int tammax;
-};
-
-
-
-
-
-
-
-class  CVectorSinTemplate {
-
+class CVector {
 public:
-    CVectorSinTemplate(int delta = 10) {
+    CVector(int delta = 10) {
         tammax = delta;
-        head = new int [tammax];
+        tam = 0;
+        head = new T[tammax];
         ptr = head;
     }
+
     void expan() {
         int h = 10;
-        int* ptr1 = new int[tammax + h];
-        for (int i = 0; i < tammax; i++) 
+        T* ptr1 = new T[tammax + h];
+        for (int i = 0; i < tammax; i++)
             ptr1[i] = head[i];
         tammax += h;
         delete[] head;
         head = ptr1;
         ptr = head;
-
-
     }
-    void Insert(int  elem) {
+
+    void Insert(T elem) {
         if (tam == tammax)
             expan();
-        *(ptr + tam) = elem;
-        tam++;
-
+        head[tam++] = elem;
     }
+
     void print() {
-        int* f = head;
-        for (int i = 0; i < tam; i++) {
-            cout << *(f + i) << " ";
-        }
+        for (int i = 0; i < tam; i++)
+            cout << head[i] << " ";
+        cout << endl;
+    }
+
+private:
+    T* head;
+    T* ptr;
+    int tam;
+    int tammax;
+};
+
+class CVectorSinTemplate {
+public:
+    CVectorSinTemplate(int delta = 10) {
+        tammax = delta;
+        tam = 0;
+        head = new int[tammax];
+        ptr = head;
+    }
+
+    void expan() {
+        int h = 10;
+        int* ptr1 = new int[tammax + h];
+        for (int i = 0; i < tammax; i++)
+            ptr1[i] = head[i];
+        tammax += h;
+        delete[] head;
+        head = ptr1;
+        ptr = head;
+    }
+
+    void Insert(int elem) {
+        if (tam == tammax)
+            expan();
+        head[tam++] = elem;
+    }
+
+    void print() {
+        for (int i = 0; i < tam; i++)
+            cout << head[i] << " ";
         cout << endl;
     }
 
@@ -130,35 +111,25 @@ private:
     int tammax;
 };
 
+int main() {
+    CVector<int> vec1(10);
+    for (int i = 0; i < 20; i++)
+        vec1.Insert(i);
+    vec1.print();
 
+    CVectorSinTemplate vec2(10);
+    for (int i = 0; i < 20; i++)
+        vec2.Insert(i);
+    vec2.print();
 
+    Vector v;
+    constructor(v.m_pVect, v.m_nCount, v.m_nMax, v.m_nDelta, v.m_nMax);
+    for (int i = 0; i < 20; i++)
+        Insert(v.m_pVect, v.m_nCount, v.m_nMax, i);
+    for (int i = 0; i < v.m_nCount; i++)
+        cout << v.m_pVect[i] << " ";
+    
 
-
-
-
-
-
-
-
-int main()
-{
-    CVector<int> x(3);
-    x.Insert(1);
-    x.Insert(2);
-    x.Insert(3);
-    x.print();
-    x.Insert(4);
-    x.print();
-
-    CVectorSinTemplate gh(10);
-
-    for (int y = 0; y < 20; y++) {
-        gh.Insert(y);
-    }
-    gh.print();
-
-    Vector f();
-
-
-
+    return 0;
 }
+
